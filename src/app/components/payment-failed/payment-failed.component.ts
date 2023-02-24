a036c6581fe6e5e4362fade4dc48a72ff70ac9d8
Mohamed Hassan <mohamed>
2023-02-24 05:17:16 +0200

    edits

--- src/app/components/payment-failed/payment-failed.component.ts
+++ src/app/components/payment-failed/payment-failed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment-failed',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentFailedComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
-
+    this.router.navigate(['/cart'],{queryParams : {type:0}})
   }
-
 }
