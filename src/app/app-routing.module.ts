9958db50e9932fa2d4c55ce00ee13e62dc0fc0d2
Mohamed Hassan <mohamed>
2023-02-24 05:40:46 +0200

    edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -107,7 +107,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled',
-    scrollPositionRestoration: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    useHash:true
   })],
   exports: [RouterModule]
 })
