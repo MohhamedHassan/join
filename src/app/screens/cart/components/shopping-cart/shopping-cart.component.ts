7cb32cce583d77a52f9e5b49bbf567ab599045a5
Mohamed Hassan <mohamed>
2023-03-20 14:43:38 +0200

    edits

--- src/app/screens/cart/components/shopping-cart/shopping-cart.component.ts
+++ src/app/screens/cart/components/shopping-cart/shopping-cart.component.ts
@@ -1097,7 +1097,7 @@ export class ShoppingCartComponent implements OnInit {
               "user_name": this.notUserData?.name || '',
               "user_phone": this.notUserData?.phone || '',
               "user_email": this.notUserData?.email || '',
-              "amount": Math.floor(Number(this.total + this.shipingCharge)),
+              "amount": (Number(this.total + this.shipingCharge)),
             }).subscribe(
               response => {
                 if (response?.message) {
